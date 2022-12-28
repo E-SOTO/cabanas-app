aa21d8a1ff5355c350c8530290173a0dcc9d148e
ericksoto451 <erick_soto@dtidimensional.mx>
2022-12-27 23:47:25 -0600

    first commit

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemListContainer() {
+  return (
+    <div>
+        <h1>Bienvenido a Cabañas App</h1>
+    </div>
+  )
+}
+
+export default ItemListContainer
