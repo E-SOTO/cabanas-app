847dbd04f91a2050dbff390f0d4fed5cbb204817
ericksoto451 <erick_soto@dtidimensional.mx>
2022-12-28 00:52:11 -0600

    Se agregaron componentes

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CartWidget() {
+  return (
+    <a className="navbar-brand" href="#">
+        <img src="./assets/shopping-cart.png" alt="Bootstrap" width="30" height="25"/>
+    </a>
+  )
+}
+
+export default CartWidget
